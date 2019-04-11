8e73e2e55f8658e4650b0d82ba87e5044bcc40a8
noboru-i <ishikura.noboru@gmail.com>
2019-04-11 21:36:04 +0900

    fix build error

--- ios/Classes/GeofencingPlugin.h
+++ ios/Classes/GeofencingPlugin.h
@@ -5,7 +5,11 @@
 
 #import <CoreLocation/CoreLocation.h>
 
+typedef void (*FlutterPluginRegistrantCallback)(NSObject<FlutterPluginRegistry>* registry);
+
 @interface GeofencingPlugin : NSObject<FlutterPlugin, CLLocationManagerDelegate>
+
++ (void)setPluginRegistrantCallback:(FlutterPluginRegistrantCallback)callback;
 
 @end
 #endif
